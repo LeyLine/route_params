3b719f238479e5a99afa1fc5fc5b43b0716c925e
Adrian Llerena <a.llerena001@gmail.com>
2018-07-05 16:43:51 -0400

    Get done

--- app.js
+++ app.js
@@ -1,22 +1,39 @@
 // app.js
-const express = require('express');
+const express = require("express");
 const app = express();
-const hbs = require('hbs');
-var path = require('path');
+const hbs = require("hbs");
+var path = require("path");
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "hbs");
 
-app.get('/', function (req, res) {
-  res.render('index');
+//ROUTES
+app.get("/", function(req, res) {
+  res.render("index");
 });
 
-app.get('/search', function (req, res) {
+app.get("/search", function(req, res) {
   res.send(req.query);
 });
 
-app.get('/users/:username', function (req, res) {
+app.get("/users/:username", function(req, res) {
   res.send(req.params);
 });
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'));
+app.get("/get-user-info", (req, res) => {
+  res.render("user-info-form");
+});
+
+app.get("/display-user-info", (req, res) => {
+  let name = req.query.name;
+  let age = req.query.age;
+  let superhero = req.query.superhero;
+
+  res.send(`
+    Your name is ${name}
+    Your age is ${age}
+    Your favorite superhero is ${superhero}
+  `);
+});
+
+app.listen(3000, () => console.log("Example app listening on port 3000!"));

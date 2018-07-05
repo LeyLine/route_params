1d8b5fe6175ffcb31bde922d47220e04410c8561
Adrian Llerena <a.llerena001@gmail.com>
2018-07-05 16:07:18 -0400

    lessons done

--- app.js
+++ app.js
@@ -8,7 +8,15 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
 app.get('/', function (req, res) {
-  console.log(req);
+  res.render('index');
+});
+
+app.get('/search', function (req, res) {
+  res.send(req.query);
+});
+
+app.get('/users/:username', function (req, res) {
+  res.send(req.params);
 });
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
